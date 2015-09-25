1470333e327594e815d42f924420b16d853dcbba
Syver Enstad <syver.enstad@gmail.com>
2015-09-25 02:48:34 +0200

    Try to fix compile error with mingw x64 on windows

--- thumbs.sh
+++ thumbs.sh
@@ -216,7 +216,7 @@ esac
 case "$tbs_arch" in
 x64)
   [ $tbs_tools = msvc12 ] && cm_tools="$cm_tools Win64"
-  [ $tbs_tools = gnu -o $tbs_tools = mingw ] && c_flags+=" -m64" ;;
+  [ $tbs_tools = gnu -o $tbs_tools = mingw ] && c_flags+=" -m64 -D_FILE_OFFSET_BITS=64" ;;
 x86)
   [ $tbs_tools = gnu -o $tbs_tools = mingw ] && c_flags+=" -m32" ;;
 
